83c4dfc4a3c67278f48aa5e690561c86bff8678f
pwalker1485 <pwalker1485@icloud.com>
2017-04-12 12:39:46 +0100

    Linux permissions script upload

---
+++ SetPermissions.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+##################################################################################
+############### Set permissions for directories and files ########################
+###################### written by Phil Walker ####################################
+##################################################################################
+
+USER="username"
+GROUP="groupname"
+
+# exit this bash script if any statement returns a non-true return value
+set -e
+
+if [ -z "$1" ]; then
+    echo
+    echo "ERROR: Must pass parameter of path to directory"
+    echo "USAGE: $0 /path/to/directory/*"
+    echo
+    exit 1
+fi
+
+read -p "Okay to continue (y/n)? "
+if [ "$REPLY" != "y" ]; then
+    echo
+    echo "EXITING -- NO ACTION TAKEN."
+    echo
+    exit 1
+fi
+
+echo
+echo "Starting..."
+echo
+
+find "$1" -type d -print0 | xargs -0 chmod 755
+find "$1" -type f -print0 | xargs -0 chmod 644
+find "$1" -type d -print0 | xargs -0 chown "$USER":$GROUP"
+find "$1" -type f -print0 | xargs -0 chown "$USER":"$GROUP"
+
+echo "Done. Standard permissions and ownership now set"
+echo
+
+exit 0
