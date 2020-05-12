0f6cde0cdc18156f83a4e97dfc45215012990f3c
pwalker1485 <pwalker1485@icloud.com>
2020-05-12 23:37:50 +0100

    amended echoes

--- InstallAllAvailableUpdates.sh
+++ InstallAllAvailableUpdates.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
 
 ########################################################################
-#           Install all available updates & upgrades                   #
+#           Install all available upgrades & updates                   #
 ################### Written by Phil Walker #############################
 ########################################################################
-
-########################################################################
-#                            Variables                                 #
-########################################################################
-
-TimeDate=$(date)
 
 ########################################################################
 #                         Script starts here                           #
 ########################################################################
 
 /bin/echo "---------------------------------------------------"
-/bin/echo "*UPDATE SCRIPT*: $TimeDate"
+/bin/echo "*UPDATE SCRIPT* Start: $(date)"
 /bin/echo "---------------------------------------------------"
 
 /bin/echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 /usr/bin/apt-get -y update && /usr/bin/apt-get -y upgrade && /usr/bin/apt-get -y dist-upgrade
 
 /bin/echo "---------------------------------------------------"
-/bin/echo "*UPDATE SCRIPT*: All Updates and Upgrades completed"
+/bin/echo "*UPDATE SCRIPT* All Updates and Upgrades completed"
 /bin/echo "---------------------------------------------------"
 
 /usr/bin/apt-get -y autoremove && /usr/bin/apt-get -y autoclean && /usr/bin/apt-get -y clean
 
 /bin/echo "---------------------------------------------------"
-/bin/echo "*UPDATE SCRIPT*: All old .deb files removed and package cache cleared"
+/bin/echo "*UPDATE SCRIPT* All old .deb files removed and package cache cleared"
 
 #Rebooting system, due to some updates/upgrades requiring a reboot to complete
 /bin/echo "---------------------------------------------------"
-/bin/echo "*UPDATE SCRIPT*: Rebooting system to complete updates and upgrades..."
+/bin/echo "*UPDATE SCRIPT* Rebooting system to complete updates and upgrades..."
+/bin/echo "*UPDATE SCRIPT* Finish: $(date)"
 /sbin/shutdown -r now
 
 exit 0
